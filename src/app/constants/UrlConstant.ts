93ca47debe238b7f5e68f6ad9fdf7bffa37267e7
aparzero30 <jimcin30@gmail.com>
2024-09-14 13:27:08 +0800

    added docker file

--- src/app/constants/UrlConstant.ts
+++ src/app/constants/UrlConstant.ts
@@ -1,4 +1,10 @@
-export class UrlConstant{
-  static loginUrl = "http://localhost:8080/api/v1/auth/login";
-  static getArtist = "http://localhost:8080/api/v1/user/artist";
+export class UrlConstant {
+  // const baseUrl = "http://music-user-adapter:8080/api/v1/auth/login"
+
+  // static loginUrl = 'http://music-user-adapter:8080/api/v1/auth/login';
+  // static getArtist = 'http://music-user-adapter:8080/api/v1/user/artist';
+  // static loginUrl = '/api/v1/auth/login';
+  // static getArtist = '/api/v1/user/artist';
+  static loginUrl = 'http://localhost:80/api/v1/auth/login';
+  static getArtist = 'http://localhost:80/api/v1/user/artist';
 }
